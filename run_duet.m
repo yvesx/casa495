d134ca7e40927897627e9d70d30ee7d704709820
Yves Xie <yxi389@gmail.com>
2014-04-24 20:31:50 -0500

    up & running. algorithm is really fast..

--- run_duet.m
+++ run_duet.m
@@ -1,13 +1,19 @@
+clc
+clear all
+close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %    setp 1,2,3
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %1. analyze the signals - STFT
+%1) Create the spectrogram of the Left and Right channels.
 wlen=1024;
 timestep=512;
 numfreq=1024;
 awin=hamming(wlen);%analysis window is a Hamming window Looks like Sine on [0,pi]
-
+[x1,fs,nbits] = wavread('data/x1.wav');
+x2 = wavread('data/x2.wav');
 % tf1 tf2 look like left right channel
+% question: why is it important to two have left right channel
 tf1=tfanalysis(x1,awin,timestep,numfreq);%time-freq domain
 tf2=tfanalysis(x2,awin,timestep,numfreq);%time-freq domain
 
@@ -21,6 +27,9 @@ fmat=freq(ones(size(tf1,2),1),:)'; % why just tf1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %2.calculate alpha and delta for each t-f point
+%2) For each time/frequency compare the phase and amplitude of the left and
+%   right channels. This gives two new coordinates, instead of time-frequency 
+%   it is phase-amplitude differences.
 R21=(tf2+eps)./(tf1+eps);%time-freqratioofthemixtures
 
 %%%2.1HERE WE ESTIMATE THE RELATIVE ATTENUATION (alpha)%%%
@@ -31,6 +40,9 @@ delta=-imag(log(R21))./fmat;% imaginary part, 'delta' relative delay
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %3.calculate weighted histogram
+%3) Build a 2-d histogram (one dimension is phase, one is amplitude) where 
+%   the height at any phase/amplitude is the count of time-frequency bins that
+%   have approximately that phase/amplitude.
 p=1; q=0; %powers used to weight histogram
 tfweight=(abs(tf1).*abs(tf2)).^p.*abs(fmat).^q; %weights vector
 maxa=0.7;
@@ -63,8 +75,11 @@ mesh(linspace(-maxd,maxd,dbins),linspace(-maxa,maxa,abins),A);
 %    step 4,5,6,7
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %4.peak centers (determined from histogram) THIS IS DONE BY HUMAN.
+%4) Determine how many peaks there are in the histogram.
+%5) Find the location of each peak. 
+
 numsources=5;
-peakdelta=[- 2-2 0 2 2];
+peakdelta=[-2 -2 0 2 2];
 peakalpha=[.19 -.21 0 .19 -.21];
 
 %convert alpha to a
@@ -72,6 +87,9 @@ peaka=(peakalpha+sqrt(peakalpha.^2+4))/2;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %5.determine masks for separation
+%6) Assign each time-frequency frame to the nearest peak in phase/amplitude 
+%  space. This partitions the spectrogram into sources (one peak per source)
+
 bestsofar=Inf*ones(size(tf1));
 bestind=zeros(size(tf1));
 for i=1:length(peakalpha)
@@ -84,6 +102,10 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %6.&7.demix with ML alignment and convert to time domain
+%7) Then you create a binary mask (1 for each time-frequency point belonging to my source, 0 for all other points)
+%8) Mask the spectrogram with the mask created in step 7.
+%9) Rebuild the original wave file from 8.
+%10) Listen to the result.
 est=zeros(numsources,length(x1));%demixtures
 for i=1:numsources
     mask=(bestind==i);
@@ -92,7 +114,8 @@ for i=1:numsources
             ./(1+peaka(i)^2)).*mask],...
              sqrt(2)*awin/1024,timestep,numfreq);
     est(i,:)=esti(1:length(x1))';
+
     %add back into the demix a little bit of the mixture
     %as that eliminates most of the masking artifacts
-    soundsc(est(i,:)+0.05*x1,fs);pause;%play demixture
+    soundsc(est(i,:)+0.05*x1',fs);pause;% original code seems to have missed the transpose play demixture
 end
